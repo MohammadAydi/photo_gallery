91c043342c0869946b034494d9e579d9be822c34
C0de871 <muhammadaydi7@gmail.com>
2026-05-22 19:27:21 +0300

    -ColorsSystemTable

--- photo_gallery/MainScreen.Designer.cs
+++ photo_gallery/MainScreen.Designer.cs
@@ -45,6 +45,16 @@ partial class MainScreen {
         UpDownColorsNumber = new System.Windows.Forms.NumericUpDown();
         ColorNumberSlider = new System.Windows.Forms.TrackBar();
         ColorsNumberLabel = new System.Windows.Forms.Label();
+        RightPanel = new System.Windows.Forms.FlowLayoutPanel();
+        ColorsSystemPanel = new System.Windows.Forms.FlowLayoutPanel();
+        ColorSystemLabel = new System.Windows.Forms.Label();
+        ColorsSystemsGrid = new System.Windows.Forms.TableLayoutPanel();
+        CMYKButton = new System.Windows.Forms.RadioButton();
+        YCbCrButton = new System.Windows.Forms.RadioButton();
+        HSVButton = new System.Windows.Forms.RadioButton();
+        HLSButton = new System.Windows.Forms.RadioButton();
+        LABButton = new System.Windows.Forms.RadioButton();
+        YUVButton = new System.Windows.Forms.RadioButton();
         TobBarFlowLayout.SuspendLayout();
         TitlePanel.SuspendLayout();
         ActionButtonsPanel.SuspendLayout();
@@ -52,6 +62,9 @@ partial class MainScreen {
         ColorsNumberFlow.SuspendLayout();
         ((System.ComponentModel.ISupportInitialize)UpDownColorsNumber).BeginInit();
         ((System.ComponentModel.ISupportInitialize)ColorNumberSlider).BeginInit();
+        RightPanel.SuspendLayout();
+        ColorsSystemPanel.SuspendLayout();
+        ColorsSystemsGrid.SuspendLayout();
         SuspendLayout();
         // 
         // panel6
@@ -243,12 +256,153 @@ partial class MainScreen {
         ColorsNumberLabel.Text = "عدد الألوان";
         ColorsNumberLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
         // 
+        // RightPanel
+        // 
+        RightPanel.AutoSize = true;
+        RightPanel.BackColor = System.Drawing.Color.FromArgb(((int)((byte)128)), ((int)((byte)255)), ((int)((byte)128)));
+        RightPanel.Controls.Add(ColorsSystemPanel);
+        RightPanel.Dock = System.Windows.Forms.DockStyle.Left;
+        RightPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+        RightPanel.Location = new System.Drawing.Point(0, 87);
+        RightPanel.Name = "RightPanel";
+        RightPanel.Size = new System.Drawing.Size(204, 456);
+        RightPanel.TabIndex = 1;
+        // 
+        // ColorsSystemPanel
+        // 
+        ColorsSystemPanel.AutoSize = true;
+        ColorsSystemPanel.BackColor = System.Drawing.Color.FromArgb(((int)((byte)255)), ((int)((byte)128)), ((int)((byte)128)));
+        ColorsSystemPanel.Controls.Add(ColorSystemLabel);
+        ColorsSystemPanel.Controls.Add(ColorsSystemsGrid);
+        ColorsSystemPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+        ColorsSystemPanel.Location = new System.Drawing.Point(3, 3);
+        ColorsSystemPanel.Name = "ColorsSystemPanel";
+        ColorsSystemPanel.Size = new System.Drawing.Size(198, 98);
+        ColorsSystemPanel.TabIndex = 2;
+        // 
+        // ColorSystemLabel
+        // 
+        ColorSystemLabel.AutoSize = true;
+        ColorSystemLabel.Location = new System.Drawing.Point(102, 0);
+        ColorSystemLabel.Name = "ColorSystemLabel";
+        ColorSystemLabel.Size = new System.Drawing.Size(93, 20);
+        ColorSystemLabel.TabIndex = 0;
+        ColorSystemLabel.Text = "النظام اللوني";
+        ColorSystemLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // ColorsSystemsGrid
+        // 
+        ColorsSystemsGrid.AutoSize = true;
+        ColorsSystemsGrid.ColumnCount = 3;
+        ColorsSystemsGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.333332F));
+        ColorsSystemsGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.333332F));
+        ColorsSystemsGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.333332F));
+        ColorsSystemsGrid.Controls.Add(YUVButton, 0, 0);
+        ColorsSystemsGrid.Controls.Add(LABButton, 0, 1);
+        ColorsSystemsGrid.Controls.Add(HLSButton, 0, 1);
+        ColorsSystemsGrid.Controls.Add(CMYKButton, 1, 0);
+        ColorsSystemsGrid.Controls.Add(YCbCrButton, 0, 1);
+        ColorsSystemsGrid.Controls.Add(HSVButton, 2, 0);
+        ColorsSystemsGrid.Location = new System.Drawing.Point(3, 23);
+        ColorsSystemsGrid.Name = "ColorsSystemsGrid";
+        ColorsSystemsGrid.RowCount = 2;
+        ColorsSystemsGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+        ColorsSystemsGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+        ColorsSystemsGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+        ColorsSystemsGrid.Size = new System.Drawing.Size(192, 72);
+        ColorsSystemsGrid.TabIndex = 1;
+        ColorsSystemsGrid.Paint += tableLayoutPanel1_Paint;
+        // 
+        // CMYKButton
+        // 
+        CMYKButton.Appearance = System.Windows.Forms.Appearance.Button;
+        CMYKButton.AutoSize = true;
+        CMYKButton.Location = new System.Drawing.Point(67, 3);
+        CMYKButton.Name = "CMYKButton";
+        CMYKButton.Size = new System.Drawing.Size(58, 30);
+        CMYKButton.TabIndex = 3;
+        CMYKButton.TabStop = true;
+        CMYKButton.Text = "CMYK";
+        CMYKButton.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        CMYKButton.UseVisualStyleBackColor = true;
+        // 
+        // YCbCrButton
+        // 
+        YCbCrButton.Appearance = System.Windows.Forms.Appearance.Button;
+        YCbCrButton.AutoSize = true;
+        YCbCrButton.Dock = System.Windows.Forms.DockStyle.Fill;
+        YCbCrButton.Location = new System.Drawing.Point(67, 39);
+        YCbCrButton.Name = "YCbCrButton";
+        YCbCrButton.Size = new System.Drawing.Size(58, 30);
+        YCbCrButton.TabIndex = 1;
+        YCbCrButton.TabStop = true;
+        YCbCrButton.Text = "YCbCr";
+        YCbCrButton.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        YCbCrButton.UseVisualStyleBackColor = true;
+        // 
+        // HSVButton
+        // 
+        HSVButton.Appearance = System.Windows.Forms.Appearance.Button;
+        HSVButton.AutoSize = true;
+        HSVButton.Dock = System.Windows.Forms.DockStyle.Fill;
+        HSVButton.Location = new System.Drawing.Point(3, 3);
+        HSVButton.Name = "HSVButton";
+        HSVButton.Size = new System.Drawing.Size(58, 30);
+        HSVButton.TabIndex = 0;
+        HSVButton.TabStop = true;
+        HSVButton.Text = "HSV";
+        HSVButton.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        HSVButton.UseVisualStyleBackColor = true;
+        // 
+        // HLSButton
+        // 
+        HLSButton.Appearance = System.Windows.Forms.Appearance.Button;
+        HLSButton.AutoSize = true;
+        HLSButton.Dock = System.Windows.Forms.DockStyle.Fill;
+        HLSButton.Location = new System.Drawing.Point(131, 39);
+        HLSButton.Name = "HLSButton";
+        HLSButton.Size = new System.Drawing.Size(58, 30);
+        HLSButton.TabIndex = 4;
+        HLSButton.TabStop = true;
+        HLSButton.Text = "HLS";
+        HLSButton.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        HLSButton.UseVisualStyleBackColor = true;
+        // 
+        // LABButton
+        // 
+        LABButton.Appearance = System.Windows.Forms.Appearance.Button;
+        LABButton.AutoSize = true;
+        LABButton.Dock = System.Windows.Forms.DockStyle.Fill;
+        LABButton.Location = new System.Drawing.Point(3, 39);
+        LABButton.Name = "LABButton";
+        LABButton.Size = new System.Drawing.Size(58, 30);
+        LABButton.TabIndex = 5;
+        LABButton.TabStop = true;
+        LABButton.Text = "LAB";
+        LABButton.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        LABButton.UseVisualStyleBackColor = true;
+        // 
+        // YUVButton
+        // 
+        YUVButton.Appearance = System.Windows.Forms.Appearance.Button;
+        YUVButton.AutoSize = true;
+        YUVButton.Dock = System.Windows.Forms.DockStyle.Fill;
+        YUVButton.Location = new System.Drawing.Point(131, 3);
+        YUVButton.Name = "YUVButton";
+        YUVButton.Size = new System.Drawing.Size(58, 30);
+        YUVButton.TabIndex = 6;
+        YUVButton.TabStop = true;
+        YUVButton.Text = "YUV";
+        YUVButton.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        YUVButton.UseVisualStyleBackColor = true;
+        // 
         // MainScreen
         // 
         AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
         AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         BackColor = System.Drawing.SystemColors.Control;
         ClientSize = new System.Drawing.Size(781, 543);
+        Controls.Add(RightPanel);
         Controls.Add(TobBarFlowLayout);
         Controls.Add(TopBar);
         Location = new System.Drawing.Point(19, 19);
@@ -268,9 +422,32 @@ partial class MainScreen {
         ColorsNumberFlow.PerformLayout();
         ((System.ComponentModel.ISupportInitialize)UpDownColorsNumber).EndInit();
         ((System.ComponentModel.ISupportInitialize)ColorNumberSlider).EndInit();
+        RightPanel.ResumeLayout(false);
+        RightPanel.PerformLayout();
+        ColorsSystemPanel.ResumeLayout(false);
+        ColorsSystemPanel.PerformLayout();
+        ColorsSystemsGrid.ResumeLayout(false);
+        ColorsSystemsGrid.PerformLayout();
         ResumeLayout(false);
         PerformLayout();
     }
+
+    private System.Windows.Forms.RadioButton HLSButton;
+    private System.Windows.Forms.RadioButton YUVButton;
+
+    private System.Windows.Forms.RadioButton YCbCrButton;
+    private System.Windows.Forms.RadioButton LABButton;
+    private System.Windows.Forms.RadioButton CMYKButton;
+
+    private System.Windows.Forms.RadioButton HSVButton;
+
+    private System.Windows.Forms.TableLayoutPanel ColorsSystemsGrid;
+
+    private System.Windows.Forms.Label ColorSystemLabel;
+
+    private System.Windows.Forms.FlowLayoutPanel ColorsSystemPanel;
+
+    private System.Windows.Forms.FlowLayoutPanel RightPanel;
 
     private System.Windows.Forms.TrackBar ColorNumberSlider;
     private System.Windows.Forms.NumericUpDown UpDownColorsNumber;
